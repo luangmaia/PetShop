efb4ffae227f87c13cd7cd8b9e2c3ddb2a6f39d8
Luan Maia <luan.dias@dcomp.sor.ufscar.br>
2018-01-30 15:39:56 -0200

    Integração com Webservice

--- js/controllers/LoginCtrl.js
+++ js/controllers/LoginCtrl.js
@@ -1,3 +1,24 @@
-app.controller('LoginCtrl', function($scope) {
+app.controller('LoginCtrl', function($scope, $localStorage, $window, UserByUsername) {
+    /* Verificar se está logado */
+    if ($localStorage.userLogged != null) {
+        $window.location.href = '#/comprar';
+    }
 
+    /* Funções $scope */
+    $scope.entrarClick = function () {
+        console.log("Tentativa de login");
+        var userResource = UserByUsername.query($scope.user.username);
+
+        userResource.$promise.then(function () {
+            if ($scope.user.password === userResource.password) {
+                console.log("Login com sucesso");
+                $localStorage.userLogged = userResource;
+                $window.location.href = '#/comprar';
+            } else {
+                console.log("Senha incorreta");
+            }
+        }, function () {
+            console.log("Usuário não existe");
+        });
+    };
 });
